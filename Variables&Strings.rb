2af4c1cbe6c52268500bf38d4e040bb6def47636
Yash Sawant <65642849+YashKSawant@users.noreply.github.com>
2020-08-14 11:07:00 +0530

    Add files via upload

---
+++ Variables&Strings.rb
@@ -0,0 +1,23 @@
+#So here we will be talking about variables and also some features why this is so easy:)
+#Ruby is kind of feels like A English language ...
+puts 4+5
+#see it performs also arithmetic operations wwithout using variables
+#Unlike other PL in Ruby not necessary to use datatype for a variable it interpets it.
+str= "    Hello   "
+num=7
+#Like that
+#Also about talking about some methods ....
+puts str.upcase()
+puts str.downcase()
+puts str.strip()
+puts str.length()
+puts str.include? " World "#Checks whether this string is included or not
+puts str.index("H")
+puts str[4]
+puts str[0,8]
+puts "programming".upcase()#Also works on strings
+#I'll recommend you to "GOOGLE" -> "ruby string methods" & you will get list of all.
+#See You in next...
+puts "Pls enter your name:"
+name=gets
+puts ("Hello"+ name	)
